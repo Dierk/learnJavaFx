33de3394a6660fe6c9a19b9fc6be87a327225f6d
Dierk Koenig <dierk.koenig@canoo.com>
2013-01-08 11:33:36 +0100

    added the onMouseClicked handler

--- src/solution/Abacus_2_Add_all_balls_and_onClick.java
+++ src/solution/Abacus_2_Add_all_balls_and_onClick.java
@@ -1,6 +1,5 @@
 package solution;
 
-import javafx.animation.TranslateTransitionBuilder;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -9,9 +8,8 @@ import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.CircleBuilder;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
-public class Abacus_2_Add_all_balls extends Application {
+public class Abacus_2_Add_all_balls_and_onClick extends Application {
 
     private static final int ROW_COUNT = 10;
     private static final int COL_COUNT = 10;
@@ -19,6 +17,7 @@ public class Abacus_2_Add_all_balls extends Application {
     private static final int DIAMETER  = 2 * RADIUS;
     private static final int WIDTH     = COL_COUNT * DIAMETER;
     private static final int HEIGHT    = ROW_COUNT * DIAMETER;
+    private static final int MOVE_WAY  = 100;
 
     @Override
     public void start(Stage primaryStage) {
@@ -27,12 +26,21 @@ public class Abacus_2_Add_all_balls extends Application {
 
         for (int row = 0; row < ROW_COUNT; row++) {
             for (int column = 0; column < COL_COUNT; column++) {
-                Circle circle = makeCircle(row, column);
+                final Circle circle = makeCircle(row, column);
                 root.getChildren().add(circle);
+                onClick(circle);
             }
         }
-        primaryStage.setScene(new Scene(root, WIDTH, HEIGHT));
+        primaryStage.setScene(new Scene(root, WIDTH + MOVE_WAY, HEIGHT));
         primaryStage.show();
+    }
+
+    private void onClick(final Circle circle) {
+        circle.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent mouseEvent) {
+                circle.setTranslateX(MOVE_WAY);
+            }
+        });
     }
 
     private Circle makeCircle(int row, int column) {
