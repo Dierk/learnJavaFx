18f4ddd192bdd185e5cc950e75636b4eb6d4a231
Dierk Koenig <dierk.koenig@canoo.com>
2013-02-01 21:22:21 +0100

    add rails and text binding

---
+++ src/solution/Abacus_4_Rail_and_Text_binding.java
@@ -0,0 +1,81 @@
+package solution;
+
+import javafx.animation.TranslateTransition;
+import javafx.animation.TranslateTransitionBuilder;
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.CircleBuilder;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.RectangleBuilder;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import static javafx.util.Duration.millis;
+
+public class Abacus_4_Rail_and_Text_binding extends Application {
+
+    private static final int    ROW_COUNT    = 10;
+    private static final int    COL_COUNT    = 10;
+    private static final int    RADIUS       = 20;
+    private static final int    DIAMETER     = 2 * RADIUS;
+    private static final int    MOVE_WAY     = 8 * DIAMETER;
+    private static final int    WIDTH        = COL_COUNT * DIAMETER + MOVE_WAY;
+    private static final int    HEIGHT       = ROW_COUNT * DIAMETER;
+    private static final int    PADDING      = 20;
+    private static final int    OFFSET       = PADDING + RADIUS;
+    private static final int    RAIL_HEIGHT  = 10;
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX Abacus with Binding");
+        Pane root = new Pane();
+
+        for (int row = 0; row < ROW_COUNT; row++) {
+            Rectangle rail = RectangleBuilder.create()
+                .width(WIDTH)
+                .height(RAIL_HEIGHT)
+                .x(PADDING)
+                .y(OFFSET - (RAIL_HEIGHT / 2) + (row * DIAMETER))
+                .build();
+            root.getChildren().add(rail);
+            for (int column = 0; column < COL_COUNT; column++) {
+                final Circle circle = makeCircle(OFFSET + (row * DIAMETER), OFFSET + (column * DIAMETER));
+                root.getChildren().add(circle);
+
+                Text text = new Text(circle.getCenterX()-3, circle.getCenterY()+4, ""+ ((COL_COUNT - column) % COL_COUNT));
+                text.translateXProperty().bind(circle.translateXProperty());
+                text.setOnMouseClicked(circle.getOnMouseClicked());
+                text.setFill(Color.WHITE);
+                root.getChildren().add(text);
+            }
+        }
+        primaryStage.setScene(new Scene(root, WIDTH + 2 * PADDING, HEIGHT + 2 * PADDING));
+        primaryStage.show();
+    }
+
+    private Circle makeCircle(int y, int x) {
+        final Circle ball = CircleBuilder.create().radius(RADIUS-1).centerX(x).centerY(y).build();
+        ball.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent mouseEvent) {
+                double newX = MOVE_WAY;
+                if (ball.getTranslateX() > 1) {
+                    newX = 0;
+                }
+                TranslateTransition move = TranslateTransitionBuilder.create().node(ball).toX(newX).duration(millis(200)).build();
+                move.playFromStart();
+            }
+        });
+        return ball;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
